83cffb5aca4e1c5981b1f15731ed668a92c2b28f
Sami53NP41 <samuil.stanchev@pgmett.com>
2023-02-22 13:01:59 +0200

    add code

---
+++ foreac.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace foreac
+
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        string message = "1234spiashata#$Q%& krastavica";
+
+        char symbol = '&';
+
+        int result = (int)message[4];
+        Console.WriteLine($"Letter 2*(is {(char)('(' + '(')} letter from");
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            Console.Write(message[i] + " ; ");
+        }
+
+        foreach (char letter in message)
+        {
+            Console.WriteLine(letter + " - " + (int)letter);
+        }
+    }
+}
+}
---
+++ probka.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace probka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+            for (int i = 0; i < number.Length; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+            }
+
+            if (number.Length > 0)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
