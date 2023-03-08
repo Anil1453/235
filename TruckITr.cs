c600955a8bd4121fe4dacfda5c5832c3c84311a0
Sami53NP41 <samuil.stanchev@pgmett.com>
2023-03-08 13:51:39 +0200

    add

---
+++ TruckITr.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Startup
+    {
+        static void Main(string[] args)
+        {
+            var tr1 = new Parking.Truck();
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            tr2.Go();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+namespace Parking
+{
+    internal class Truck
+    {
+        internal void Go()
+        {
+          
+           Console.WriteLine("ok");
+        }
+    }
+}
+
+namespace Vehicles
+{
+    internal class Truck
+    {
+        internal void Go()
+        {
+
+            Console.WriteLine("running");
+        }
+    }
+}
