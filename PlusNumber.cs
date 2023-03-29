6b3f6f4dc96893ab136c5c8c63a564ba55046542
Sami53NP41 <samuil.stanchev@pgmett.com>
2023-03-29 13:45:00 +0300

    new codes

---
+++ PlusNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlusNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TopInteger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int current = numbers[i];
+                    bool isGoodNumber= true;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (j > i)
+                    {
+                        Console.WriteLine(j); ;
+                    }
+                }
+            }
+        }
+    }
+}
